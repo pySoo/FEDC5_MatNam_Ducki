4e9a70a0d007502ac5a48dbe96ff7d7e2cdf74f6
pySoo <soopy368@gmail.com>
2024-04-15 23:58:30 +0900

    [✨feat]: 스크롤 성능 최적화를 위한 useThrottle 훅 추가

---
+++ src/hooks/useThrottle.ts
@@ -0,0 +1,25 @@
+import { useRef } from 'react';
+
+export const useThrottle = (callback: () => void, delay?: number) => {
+  const isWaiting = useRef(false);
+
+  const timerId = useRef<ReturnType<typeof setTimeout>>();
+  const timeDelay = delay ?? 500;
+
+  return () => {
+    if (!isWaiting.current) {
+      callback();
+      isWaiting.current = true;
+
+      setTimeout(() => {
+        isWaiting.current = false;
+      }, timeDelay);
+    }
+
+    if (timerId.current) clearTimeout(timerId.current);
+
+    timerId.current = setTimeout(() => {
+      callback();
+    }, timeDelay);
+  };
+};
