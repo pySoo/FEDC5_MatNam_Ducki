92a37d90dcb4e23fe1be2a3e69d729d7c7383580
pySoo <soopy368@gmail.com>
2024-04-16 23:53:49 +0900

    [✨feat]: 스크롤 유지 Container 컴포넌트 추가

--- src/components/Common/KeepScrollContainer/index.tsx
+++ src/components/Common/KeepScrollContainer/index.tsx
@@ -1,11 +1,43 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useRecoilState } from 'recoil';
 
-import { useKeepScroll } from '@/hooks/useKeepScroll';
+import { useThrottle } from '@/hooks/useThrottle';
+import { scrollAtom } from '@/recoil/scroll';
+import { PATH } from '@/routes/path';
 
 import { ScrollWrapper } from './style';
 
 export default function KeepScrollContainer({ children }: PropsWithChildren) {
-  const { scrollRef } = useKeepScroll();
+  const [scrollState, setScrollState] = useRecoilState(scrollAtom);
+  const scrollRef = useRef<HTMLDivElement>(null);
+
+  const { pathname } = useLocation();
+  const isKeepScrollPath = pathname === PATH.ROOT;
+
+  const handleScroll = useThrottle(() => {
+    if (scrollRef.current === null) return;
+
+    setScrollState({
+      x: 0,
+      y: scrollRef.current.scrollTop,
+    });
+  });
+
+  useEffect(() => {
+    if (scrollRef.current === null) return;
+
+    if (isKeepScrollPath) {
+      scrollRef.current.scrollTo(0, scrollState.y);
+
+      scrollRef.current.addEventListener('scroll', handleScroll);
+      return () => {
+        scrollRef.current?.removeEventListener('scroll', handleScroll);
+      };
+    } else {
+      scrollRef.current.scrollTo(0, 0);
+    }
+  }, [scrollRef, pathname]);
 
   return <ScrollWrapper ref={scrollRef}>{children}</ScrollWrapper>;
 }
